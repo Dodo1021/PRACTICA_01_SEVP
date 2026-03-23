827c7c04571da8c4bc6e6e3395c5d967855f9ac0
PachVsus <jesus.vargas@iteso.mx>
2026-03-23 15:11:13 -0600

    Cambia suma por resta en script.js dentro de fix_1

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,5 @@ alert("Hola desde script.js");
 
 var a = 10;
 var b = 5;
-var suma = a + b;
-console.log("La suma de " + a + " + " + b + " = " + suma);
+var resta = a - b;
+console.log("La suma de " + a + " - " + b + " = " + resta);
